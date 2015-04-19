3880f6abdef1803de44bf029f11468ac56855a65
Luong Hoang <lhoang@live.com>
2015-04-19 01:13:09 -0400

    redesigned based on default AOSP keyboard UI

--- app/src/main/java/com/example/android/softkeyboard/LatinKeyboard.java
+++ app/src/main/java/com/example/android/softkeyboard/LatinKeyboard.java
@@ -212,7 +212,7 @@ public class LatinKeyboard extends Keyboard {
                 mEnterKey.label = res.getText(R.string.label_send_key);
                 break;
             default:
-                mEnterKey.icon = res.getDrawable(R.drawable.sym_keyboard_return);
+                mEnterKey.icon = res.getDrawable(R.drawable.sym_keyboard_return_holo_dark);
                 mEnterKey.label = null;
                 break;
         }
