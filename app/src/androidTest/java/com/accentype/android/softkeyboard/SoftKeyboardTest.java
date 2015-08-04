e2ec8aebd6938be6012f391d493eec6ca99578fc
Luong Hoang <lhoang@live.com>
2015-08-04 11:45:09 -0400

    experimenting with service tests

---
+++ app/src/androidTest/java/com/accentype/android/softkeyboard/SoftKeyboardTest.java
@@ -0,0 +1,62 @@
+package com.accentype.android.softkeyboard;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class SoftKeyboardTest extends ServiceTestCase<SoftKeyboard> {
+
+    public SoftKeyboardTest() {
+        super(SoftKeyboard.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        Intent startIntent = new Intent();
+        startIntent.setClass(getContext(), SoftKeyboard.class);
+        startService(startIntent);
+    }
+
+    /**
+     * The name 'test preconditions' is a convention to signal that if this
+     * test doesn't pass, the test case was not set up properly and it might
+     * explain any and all failures in other tests.  This is not guaranteed
+     * to run before other tests, as junit uses reflection to find the tests.
+     */
+    @SmallTest
+    public void testPreconditions() {
+    }
+
+    /**
+     * Test basic startup/shutdown of Service
+     */
+    @SmallTest
+    public void testStartable() {
+        Intent startIntent = new Intent();
+        startIntent.setClass(getContext(), SoftKeyboard.class);
+        startService(startIntent);
+    }
+
+    /**
+     * Test binding to service
+     */
+    @MediumTest
+    public void testBindable() {
+        Intent startIntent = new Intent();
+        startIntent.setClass(getContext(), SoftKeyboard.class);
+        IBinder service = bindService(startIntent);
+    }
+
+    /**
+     * Test on key
+     */
+    @LargeTest
+    public void testOnKey() {
+
+    }
+}
